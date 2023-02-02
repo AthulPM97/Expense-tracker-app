5fc22e0d03738b9629e3dbdf7aa5e490414c7d35
athulpm <97athulpm@gmail.com>
2023-02-02 12:09:27 +0530

    Wrote test suite for forgot password screen

---
+++ src/components/Authentication/ForgotPassword.test.js
@@ -0,0 +1,28 @@
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import ForgotPassword from "./ForgotPassword";
+
+describe('forgot password page', () => {
+    test('show enter email form', () => {
+        //Arrange
+        render(<ForgotPassword/>);
+
+        //Act
+
+        //Assert
+        const outputPrompt = screen.getByText('Enter registered email', {exact: false});
+        expect(outputPrompt).toBeInTheDocument();
+    });
+    test('show loading when "send link" button clicked', () => {
+        //Arrange
+        render(<ForgotPassword/>);
+
+        //Act
+        const buttonElement = screen.getByRole('button');
+        userEvent.click(buttonElement);
+
+        //Assert
+        const loadingElement = screen.getByText('Loading...', {exact: false});
+        expect(loadingElement).toBeInTheDocument();
+    })
+});
